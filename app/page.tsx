ada73a2ae83602c2cf1d3310568f5e8a1eac0503
b.aryee <b.aryee@generisdevelopers.com>
2026-05-19 11:24:21 +0000

    Remove Pricing

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <StackedCards />
       <WhyChooseUs />
       <Testimonials />
-      <Pricing />
+      {/*<Pricing />*/}
       <FAQ />
       <CTA />
       <Contact />
